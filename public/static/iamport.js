9a5c53d77636bd1b1603109da439ee2397cffb57
Sonik Jhang <sonik@everyonesocial.com>
2020-05-27 00:00:41 -0500

    add iamport

--- public/static/iamport.js
+++ public/static/iamport.js
@@ -1,24 +1,4 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document'
-import {ServerStyleSheet} from 'styled-components'
-
-export default class MyDocument extends Document {
-  static getInitialProps({renderPage}) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return {...page, styleTags};
-  }
-
-  render() {
-    return (
-      <html>
-      <Head>
-        {this.props.styleTags}
-        <link href="https://use.fontawesome.com/releases/v5.2.0/css/svg-with-js.css" rel="stylesheet"/>
-        <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.1.5.js"/>
-        <script dangerouslySetInnerHTML={{__html: `(function() {
+(function() {
   var IMP = window.IMP;
   IMP.init('imp71021771');
 
@@ -65,13 +45,4 @@ export default class MyDocument extends Document {
       alert(msg);
     }
   }
-})`}}/>
-      </Head>
-      <body>
-      <Main/>
-      <NextScript/>
-      </body>
-      </html>
-    );
-  }
-}
+})
